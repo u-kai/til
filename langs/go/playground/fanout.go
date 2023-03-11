42635cd8edaf32d760cf466f923fb9b18c6921c5
u-kai <76635578+u-kai@users.noreply.github.com>
2023-03-11 19:50:47 +0900

    add devops 11

--- langs/go/playground/fanout.go
+++ langs/go/playground/fanout.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"math/rand"
-	"time"
-)
-
 func main() {
-	rand := func() interface{} { return rand.Intn(500000000) }
-	done := make(chan interface{})
-	defer close(done)
 
-	start := time.Now()
 }
