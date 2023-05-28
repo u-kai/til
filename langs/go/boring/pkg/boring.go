ee745efb27825c6e8d1d8055bfd6dc9fa5e234ba
u-kai <76635578+u-kai@users.noreply.github.com>
2023-05-28 22:42:49 +0900

    add throw

--- langs/go/boring/pkg/boring.go
+++ langs/go/boring/pkg/boring.go
@@ -1,6 +1,9 @@
 package pkg
 
+import "fmt"
+
 type Boring struct {
+	flame int
 	score int
 }
 
@@ -8,10 +11,55 @@ func NewBoring() *Boring {
 	return &Boring{}
 }
 
-func (b *Boring) Slow(i int) {
+func (b *Boring) Slow(i int) error {
+	if b.flame >= 20 {
+		return fmt.Errorf("flame is %d", b.flame)
+	}
+	b.flame++
 	b.score += i
+	return nil
 }
 
 func (b *Boring) Score() int {
 	return b.score
+}
+
+type Frame interface {
+	Score() int
+}
+
+type NormalFrame struct {
+	firstThrow  int
+	secondThrow int
+	throw       bool
+}
+
+func NewFlame() *NormalFrame {
+	return &NormalFrame{}
+}
+
+func (f *NormalFrame) Slow(i int) error {
+	if i > 10 {
+		return fmt.Errorf("throw is %d", i)
+	}
+	if !f.throw {
+		f.firstThrow = i
+		f.throw = true
+		return nil
+	}
+	if f.isSecondThrow() {
+		f.secondThrow = i
+	}
+	return fmt.Errorf("throw is %d", i)
+}
+func (f *NormalFrame) isSecondThrow() bool {
+	return f.throw && !f.isStrike()
+}
+
+func (f *NormalFrame) isStrike() bool {
+	return f.firstThrow == 10
+}
+
+func (n *NormalFrame) Score() int {
+	return n.firstThrow + n.secondThrow
 }
--- langs/go/boring/pkg/boring_test.go
+++ langs/go/boring/pkg/boring_test.go
@@ -5,8 +5,30 @@ import (
 	"testing"
 )
 
-func TestScoreCanBeCalculated(t *testing.T) {
+//func TestIfGetASpareThenNextThrowScoreWillBeAdded(t *testing.T) {
+//sut := pkg.NewBoring()
+//sut.Slow(1)
+//sut.Slow(9)
+//sut.Slow(1)
+//result := sut.Score()
 
+//if result != 12 {
+//t.Errorf("Expected 12, got %d", result)
+//}
+//}
+
+func TestIfNotStrikeInOneFramePlayerWillThrowTwice(t *testing.T) {
+	sut := pkg.NewFlame()
+	sut.Slow(1)
+	sut.Slow(2)
+	result := sut.Score()
+
+	if result != 3 {
+		t.Errorf("Expected 3, got %d", result)
+	}
+}
+
+func TestScoreCanBeCalculated(t *testing.T) {
 	sut := pkg.NewBoring()
 	sut.Slow(1)
 	sut.Slow(3)
@@ -15,14 +37,25 @@ func TestScoreCanBeCalculated(t *testing.T) {
 	if result != 4 {
 		t.Errorf("Expected 4, got %d", result)
 	}
-
 	sut.Slow(4)
 	sut.Slow(5)
-
 	result = sut.Score()
-
 	if result != 13 {
 		t.Errorf("Expected 13, got %d", result)
 	}
+}
 
+func TestBoardFrameIsLimitedTo10Times(t *testing.T) {
+	sut := pkg.NewBoring()
+	for i := 0; i < 20; i++ {
+		err := sut.Slow(1)
+		if err != nil {
+			t.Errorf("Expected no error, got %v", err)
+		}
+	}
+	err := sut.Slow(1)
+
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
 }
