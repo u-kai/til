dd26c2571cab7866ca47164de6d81c817b86df25
u-kai <76635578+u-kai@users.noreply.github.com>
2023-06-11 11:46:55 +0900

    add desc-copilot

--- ai/sample-ts.ts
+++
@@ -1,9 +0,0 @@
-function isPrime(n: number): boolean {
-  // A prime number is a number that is divisible only by itself and one
-  // Return true if n is prime, false otherwise
-  // This function is not optimized for speed
-  for (let i = 2; i < n; i++) {
-    if (n % i == 0) return false;
-  }
-  return true;
-}
