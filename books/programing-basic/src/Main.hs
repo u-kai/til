ff6a1f28b2b1e0b8612679183efbee4599cadfd7
u-kai <76635578+u-kai@users.noreply.github.com>
2023-07-01 15:23:31 +0900

    add sre

--- books/programing-basic/src/Main.hs
+++ books/programing-basic/src/Main.hs
@@ -1,8 +1,25 @@
 module Main where
 
-add ::  Int ->  Int -> Int
-add x y = x + y 
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+
+newtype Circle = Circle {
+    r :: Float
+}
+
+bmiTell :: Double ->  String
+bmiTell bmi
+    | bmi <= 18.5 = "You're underweight, you emo, you!"
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+    | otherwise   = "You're a whale, congratulations!"
+
 main :: IO ()
 main = do
-    let result =  add 1 2
-    print (show result)
+    print(factorial 0)
+    print(factorial 1)
+    print(factorial 2)
+    print(factorial 3)
+    print(factorial 4)
