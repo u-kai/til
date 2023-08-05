78b6abf12e30b959401f9f34079c4e4f2bb10f97
u-kai <76635578+u-kai@users.noreply.github.com>
2023-08-05 10:02:24 +0900

    add env

--- wasm/game/src/lib.rs
+++ wasm/game/src/lib.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 use web_sys::console;
-
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -10,15 +10,14 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     // This provides better error messages in debug mode.
     // It's disabled in release mode so it doesn't bloat up the file size.
-    #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
+    let window = web_sys::window().unwrap();
 
     // Your code goes here!
     console::log_1(&JsValue::from_str("Hello world!"));
