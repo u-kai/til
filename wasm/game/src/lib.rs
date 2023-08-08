fa44222f55e69d1a2e01636f62c747cf815e1b7d
u-kai <76635578+u-kai@users.noreply.github.com>
2023-08-09 07:01:52 +0900

    add success run

--- wasm/game/src/lib.rs
+++ wasm/game/src/lib.rs
@@ -59,42 +59,35 @@ pub fn main_js() -> Result<(), JsValue> {
         image.set_onload(Some(callback.as_ref().unchecked_ref()));
         image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
         image.set_src("images/rhb.png");
+        let mut frame = -1;
+        let interval_callback = Closure::wrap(Box::new(move || {
+            frame = (frame + 1) % 8;
+            let frame_name = format!("Run ({}).png", frame + 1);
+            context.clear_rect(0.0, 0.0, 600.0, 600.0);
+            let sprite = sheet
+                .frames
+                .get(&frame_name)
+                .expect("Could not find sprite");
+            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                &image,
+                sprite.frame.x as f64,
+                sprite.frame.y as f64,
+                sprite.frame.w as f64,
+                sprite.frame.h as f64,
+                300.0,
+                300.0,
+                sprite.frame.w as f64,
+                sprite.frame.h as f64,
+            );
+        }) as Box<dyn FnMut()>);
+        window
+            .set_interval_with_callback_and_timeout_and_arguments_0(
+                interval_callback.as_ref().unchecked_ref(),
+                50,
+            )
+            .unwrap();
+        interval_callback.forget();
         rx.await.unwrap();
-        let sprite = sheet
-            .frames
-            .get("Run (1).png")
-            .expect("Could not find sprite");
-        context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-            &image,
-            sprite.frame.x as f64,
-            sprite.frame.y as f64,
-            sprite.frame.w as f64,
-            sprite.frame.h as f64,
-            300.0,
-            300.0,
-            sprite.frame.w as f64,
-            sprite.frame.h as f64,
-        );
-        let (tx, rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
-        let success_tx = Rc::new(Mutex::new(Some(tx)));
-        let error_tx = Rc::clone(&success_tx);
-        let image = web_sys::HtmlImageElement::new().unwrap();
-        let callback = Closure::once(move || {
-            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                success_tx.send(Ok(())).unwrap();
-            };
-        });
-        let error_callback = Closure::once(move |err: JsValue| {
-            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                web_sys::console::log_1(err.as_ref());
-                error_tx.send(Err(err)).unwrap();
-            };
-        });
-        image.set_onload(Some(callback.as_ref().unchecked_ref()));
-        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-        image.set_src("images/Idle (1).png");
-        rx.await.unwrap();
-        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
     });
     Ok(())
 }
