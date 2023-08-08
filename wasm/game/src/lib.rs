8db1dda30b39bdd1f445ca98aff7f5f3803e9846
u-kai <76635578+u-kai@users.noreply.github.com>
2023-08-08 22:23:05 +0900

    remove triangle

--- wasm/game/src/lib.rs
+++ wasm/game/src/lib.rs
@@ -36,12 +36,6 @@ pub fn main_js() -> Result<(), JsValue> {
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
-    sierpinski(
-        &context,
-        [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-        (0, 255, 0),
-        7,
-    );
     wasm_bindgen_futures::spawn_local(async move {
         let json = fetch_json("images/rhb.json")
             .await
@@ -126,66 +120,4 @@ async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
     let resp_value = wasm_bindgen_futures::JsFuture::from(window.fetch_with_str(json_path)).await?;
     let resp: web_sys::Response = resp_value.dyn_into()?;
     wasm_bindgen_futures::JsFuture::from(resp.json()?).await
-}
-
-fn midpoint(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
-    ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
-}
-
-fn draw_triangle(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-) {
-    let color = format!("rgb({},{},{})", color.0, color.1, color.2);
-    context.set_fill_style(&JsValue::from_str(&color));
-    let [top, left, right] = points;
-    context.move_to(top.0, top.1);
-    context.begin_path();
-    context.line_to(left.0, left.1);
-    context.line_to(right.0, right.1);
-    context.line_to(top.0, top.1);
-    context.close_path();
-    context.stroke();
-    context.fill();
-}
-
-fn sierpinski(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-    depth: u8,
-) {
-    draw_triangle(&context, points, color);
-    let depth = depth - 1;
-    let [top, left, right] = points;
-    if depth > 0 {
-        let mut rng = thread_rng();
-        let next_color = (
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-        );
-        let left_middle = midpoint(top, left);
-        let right_middle = midpoint(top, right);
-        let bottom_middle = (top.0, right.1);
-        sierpinski(
-            &context,
-            [top, left_middle, right_middle],
-            next_color,
-            depth,
-        );
-        sierpinski(
-            &context,
-            [left_middle, left, bottom_middle],
-            next_color,
-            depth,
-        );
-        sierpinski(
-            &context,
-            [right_middle, bottom_middle, right],
-            next_color,
-            depth,
-        );
-    }
 }
